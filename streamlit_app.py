95a7648d0f7c2355fac1ede6ad6d00ca605410f9
Vedant Pawar <vedthp06@gmail.com>
2023-08-05 18:48:53 +0530

    Title update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from hugchat.login import Login
 
 st.set_page_config(page_title="🤗 FeelBot")
 with st.sidebar:
-    st.title('🤗💬 HugChat')
+    st.title('🤗 FeelBot')
     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
         st.success('HuggingFace Login credentials already provided!', icon='✅')
         hf_email = st.secrets['EMAIL']
@@ -16,7 +16,6 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
