7566935bdbd8ac695d70f0a7c233bd307c0d4b3e
Vedant Pawar <vedthp06@gmail.com>
2023-08-05 18:57:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,19 @@ from hugchat import hugchat
 from hugchat.login import Login
 
 st.set_page_config(page_title="🤗 FeelBot")
-with st.sidebar:
-    st.title('🤗 FeelBot')
-    if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
-        st.success('HuggingFace Login credentials already provided!', icon='✅')
-        hf_email = st.secrets['EMAIL']
-        hf_pass = st.secrets['PASS']
-    else:
-        hf_email = st.text_input('Enter E-mail:', type='password')
-        hf_pass = st.text_input('Enter password:', type='password')
-        if not (hf_email and hf_pass):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+# with st.sidebar:
+#     st.title('🤗 FeelBot')
+#     if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+#         st.success('HuggingFace Login credentials already provided!', icon='✅')
+#         hf_email = st.secrets['EMAIL']
+#         hf_pass = st.secrets['PASS']
+#     else:
+#         hf_email = st.text_input('Enter E-mail:', type='password')
+#         hf_pass = st.text_input('Enter password:', type='password')
+#         if not (hf_email and hf_pass):
+#             st.warning('Please enter your credentials!', icon='⚠️')
+#         else:
+#             st.success('Proceed to entering your prompt message!', icon='👉')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
